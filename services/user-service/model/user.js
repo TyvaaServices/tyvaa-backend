a7beba3d4724154a496e9d8c24ae53986e32fd3e
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-05-13 00:52:50 +0100

    feat: extend User model with isDriver, driverLicence, and carImage fields

--- services/user-service/model/user.js
+++ services/user-service/model/user.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const User = sequelize.define('User',{
+const User = sequelize.define('User', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -13,10 +13,20 @@ const User = sequelize.define('User',{
         unique: true,
         validate: {
             is: /^\+?[1-9]\d{1,14}$/, // E.164 format
-        },
+        }
     },
-   
-})
+    isDriver: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    driverLicence: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    carImage: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+});
 
 module.exports = User;
-
