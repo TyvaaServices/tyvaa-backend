9f0048d779cdc3aa32e5e6288a1b1711f6609ab8
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-05-13 16:57:20 +0100

    feat: add Ride model with attributes for ride details

---
+++ services/ride-service/model/ride.js
@@ -0,0 +1,36 @@
+const sequelize = require('sequelize');
+
+const ride = sequelize.define('Ride', {
+    driverId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    departure: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    destination: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    dateTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    places: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+        },
+    },
+    comment: {
+        type: DataTypes.TEXT,
+        defaultValue: '',
+    },
+    price: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+module.exports = Ride;
