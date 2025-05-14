09547bf172c08b5873d7cc1dd9f99d764718778f
Cheikh Tidiane TRAORÉ <cheikh.ahmed.tijani@teksi.ca>
2025-05-14 14:38:57 +0000

    feat: add rate limiting and compression middleware to server configuration

--- services/gateway-service/server.js
+++ services/gateway-service/server.js
@@ -1,17 +1,40 @@
 'use strict';
 require('dotenv').config();
 
-const fastify = require('fastify')({logger: true});
+const fastify = require('fastify')({logger: true, trustProxy: true});
 const proxy = require('@fastify/http-proxy');
 const swaggerConfig = require('./config/swagger');
+const rateLimit = require('@fastify/rate-limit');
+
+const compress = require('@fastify/compress');
+
+fastify.register(compress, {global: true});
 
 fastify.register(require('@fastify/swagger'), swaggerConfig.options);
 fastify.register(require('@fastify/swagger-ui'), swaggerConfig.uiOptions);
 
+fastify.register(rateLimit, {
+    max: 100,
+    timeWindow: '1 minute',
+    keyGenerator: (req, res) => {
+        return req.headers['x-forwarded-for'] || req.ip;
+    },
+})
 fastify.register(proxy, {
-    upstream: process.env.CHATBOT_SERVICE_URL || 'http://localhost:2001/api/v1',
+    upstream: process.env.CHATBOT_SERVICE_URL || 'http://localhost:2001',
     prefix: '/support/chat',
-    rewritePrefix: '/support/chat',
+    rewritePrefix: '/api/v1/support/chat',
+});
+
+fastify.register(proxy, {
+    upstream: process.env.USER_SERVICE_URL || 'http://localhost:2003',
+    prefix: '/users',
+    rewritePrefix: '/api/v1/users',
+});
+fastify.register(proxy, {
+    upstream: process.env.RIDE_SERVICE_URL || 'http://localhost:2006',
+    prefix: '/rides',
+    rewritePrefix: '/api/v1/rides'
 });
 
 const port = process.env.PORT || 2000;
