b675ebfd661561f180c222b4e9efef193781476c
traorecheikh <atidiane741@gmail.com>
2025-05-14 19:48:23 +0000

    feat: add token verification middleware for authentication

--- services/chatbot-service/server.js
+++ services/chatbot-service/server.js
@@ -4,12 +4,17 @@ require('dotenv').config();
 const fastify = require('fastify')({logger: true});
 const chatbotRoutes = require('./src/routes/chatbotRoutes');
 
-fastify.register(chatbotRoutes, {prefix: '/api/v1/support'});
+
+//TODO a faire dans tout les service rend lacces avec authentification obligatoire
+// const authMiddleware = require('./src/middleware/authMiddleware');
+// fastify.register(authMiddleware);
+// fastify.addHook('onRequest', authMiddleware); fastify.register(chatbotRoutes, {prefix: '/api/v1/support'});
 
 const port = process.env.PORT || 2001;
 fastify.listen({port, host: '0.0.0.0'}, (err, address) => {
     if (err) {
         fastify.log.error(err);
+
         process.exit(1);
     }
     fastify.log.info(`Chatbot API listening at ${address}/api/support/chat`);
