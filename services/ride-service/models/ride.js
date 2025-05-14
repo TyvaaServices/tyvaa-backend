ab5cc86558241015926ec67d7114c95c957dc9b0
Houleymatou DIALLO <dhouleymatou150@gmail.com>
2025-05-14 01:51:32 +0100

    refactor: remove unnecessary whitespace in ride model file

--- services/ride-service/models/ride.js
+++ services/ride-service/models/ride.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-
 const ride = sequelize.define('Ride', {
     driverId: {
         type: DataTypes.INTEGER,
