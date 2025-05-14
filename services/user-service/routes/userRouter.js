e1fdb473ed998c20c7a97f537bae6e391e5a81e1
Cheikh Tidiane TRAORÉ <cheikh.ahmed.tijani@teksi.ca>
2025-05-14 14:38:46 +0000

    feat: update user routes to include specific endpoints for login and registration

--- services/user-service/routes/userRouter.js
+++ services/user-service/routes/userRouter.js
@@ -3,9 +3,9 @@ const userController = require('./../controllers/userController');
 async function userRoutes(fastify, opts) {
     fastify.get('/health', userController.health);
     fastify.get('/users', userController.getAllUsers);
-    fastify.post('/login', userController.login);
+    fastify.post('/users/login', userController.login);
     fastify.get('/users/:id', userController.getUserById);
-    fastify.post('/users', userController.createUser);
+    fastify.post('/users/register', userController.createUser);
     fastify.put('/users/:id', userController.updateUser);
     fastify.delete('/users/:id', userController.deleteUser);
     fastify.post('/users/:id/fcm-token', userController.updateFcmToken);
