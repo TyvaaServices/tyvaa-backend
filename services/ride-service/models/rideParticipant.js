69a620a3b4c5cfdf6d44008b52ba47b2fb117665
Cheikh Tidiane TRAORÉ <cheikh.ahmed.tijani@teksi.ca>
2025-05-15 14:46:52 +0000

    feat: add RideParticipant model for managing ride participant data

---
+++ services/ride-service/models/rideParticipant.js
@@ -0,0 +1,18 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('./../config/db');
+
+const RideParticipant = sequelize.define('RideParticipant', {
+    rideId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'accepted', 'rejected', 'cancelled'),
+        defaultValue: 'pending',
+    }
+});
+module.exports = RideParticipant;
