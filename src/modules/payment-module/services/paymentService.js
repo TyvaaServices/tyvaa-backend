a73e26e7903fbac5375fdd3a0d6c7adfe59e54eb
diallohouleymatou <dhouleymatou150@gmail.com>
2025-06-15 12:01:24 -0400

    feat: add payment service to generate payment links

---
+++ src/modules/payment-module/services/paymentService.js
@@ -0,0 +1,5 @@
+function generatePaymentLink({ amount }) {
+    return `https://pay.wave.com/m/M_sn_7lpUxnUvMXtX/c/sn/?amount=${encodeURIComponent(amount)}`;
+}
+
+module.exports = { generatePaymentLink };
