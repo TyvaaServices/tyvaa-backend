c1fe2903043a4f10cf09e187e4e49704cf42d475
diallohouleymatou <dhouleymatou150@gmail.com>
2025-06-15 12:00:49 -0400

    fix: update database connection string to include a default for local development

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,11 @@ const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL||"postgres://postgres:oulimata2@localhost:5432/testage", {
     dialect: 'postgres',
     protocol: 'postgres',
     logging: true,
     sync:{alter:true}
 });
 
-module.exports = sequelize;
 module.exports = sequelize;
