317e40136ca498aa0461ff5937af3326ddf6519e
traorecheikh <atidiane741@gmail.com>
2025-06-15 16:02:48 +0000

    feat: add mailer functionality and integrate admin routes in server setup

--- src/modules/user-module/server.js
+++ src/modules/user-module/server.js
@@ -1,6 +1,9 @@
-const router = require('./routes/userRouter');
-require('dotenv').config();
+const userRoutes = require("./routes/userRouter");
+const adminRoutes = require("./routes/adminRouter");
+
+require("dotenv").config();
 module.exports = async function (fastify, opts) {
-    fastify.register(require('@fastify/multipart'));
-    fastify.register(router, { prefix: '/api/v1' });
+  fastify.register(require("@fastify/multipart"));
+  fastify.register(userRoutes, { prefix: "/api/v1" });
+  fastify.register(adminRoutes, { prefix: "/api/v1" });
 };
