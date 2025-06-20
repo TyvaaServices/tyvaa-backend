3714aea8da08472251c06912e45d872a54476bb0
traorecheikh <atidiane741@gmail.com>
2025-06-20 17:49:21 +0000

    feat: update chatbot routes and tests for improved error handling and module imports

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+export default {
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.js'],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.js',
+        '!**/node_modules/**',
+    ],
+    coverageReporters: ['text', 'lcov'],
+    setupFiles: ['./tests/setup.js'],
+    transform: {},
+};
