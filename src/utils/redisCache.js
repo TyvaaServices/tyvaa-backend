fcd962b55598472d62e0988f1d402e1972fb6f78
traorecheikh <atidiane741@gmail.com>
2025-06-20 13:57:28 +0000

    feat: implement audit logging functionality with CRUD operations and validation

---
+++ src/utils/redisCache.js
@@ -0,0 +1,39 @@
+const { Redis } = require('@upstash/redis');
+
+class RedisCache {
+    constructor() {
+        if (!RedisCache.instance) {
+            this.redis = Redis.fromEnv();
+            RedisCache.instance = this;
+        }
+        return RedisCache.instance;
+    }z
+
+    async get(key) {
+        const data = await this.redis.get(key);
+        if (!data) return null;
+        return JSON.parse(data);
+    }
+
+    async set(key, value, ttl = 60) {
+        const json = JSON.stringify(value);
+        return await this.redis.set(key, json, { ex: ttl });
+    }
+
+    async del(key) {
+        return await this.redis.del(key);
+    }
+
+    async getModel(key, Model) {
+        const json = await this.redis.get(key);
+        if (!json) return null;
+
+        const obj = JSON.parse(json);
+        if (Model) {
+            return Model.build(obj, { isNewRecord: false });
+        }
+        return obj;
+    }
+}
+
+module.exports = new RedisCache();
