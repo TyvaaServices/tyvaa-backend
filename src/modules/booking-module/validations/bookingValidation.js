2d6bec462b078fceef58169d21fba8c6507af0e9
traorecheikh <atidiane741@gmail.com>
2025-07-02 14:09:58 +0000

    feat: implement zod for ride and booking module

---
+++ src/modules/booking-module/validations/bookingValidation.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const createBookingSchema = z.object({
+  body: z.object({
+    rideInstanceId: z.string(),
+    seatsBooked: z.number().int().positive(),
+  }),
+});
+
+export const updateBookingSchema = z.object({
+  params: z.object({
+    id: z.string(),
+  }),
+  body: z.object({
+    seatsBooked: z.number().int().positive().optional(),
+    status: z.enum(['booked', 'cancelled']).optional(),
+  }),
+});
